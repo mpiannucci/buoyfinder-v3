6081dab83c9aeb6619f5d2c1381c45378e21a959
Matthew Iannucci <mpiannucci@gmail.com>
2018-08-15 20:47:43 -0400

    Add int to float handling

--- cargo/src/lib.rs
+++ cargo/src/lib.rs
@@ -3,6 +3,7 @@
 extern crate libc;
 #[macro_use] 
 extern crate serde_derive;
+extern crate serde;
 extern crate serde_xml_rs;
 
 pub mod strings;
--- cargo/src/location.rs
+++ cargo/src/location.rs
@@ -1,17 +1,20 @@
 use std::string::String;
+use std::f64;
+use std::fmt;
+use serde::de::{self, Deserializer, Visitor, Unexpected};
 
 #[derive(Debug, Default, Clone, Serialize, Deserialize)]
 pub struct Location {
     #[serde(default)]
     pub name: String,
 
-    #[serde(rename = "lat")]
+    #[serde(rename = "lat", deserialize_with = "string_as_f64")]
     pub latitude: f64,
 
-    #[serde(rename = "lon")]
+    #[serde(rename = "lon", deserialize_with = "string_as_f64")]
     pub longitude: f64,
 
-    #[serde(rename = "elev", default)]
+    #[serde(rename = "elev", default, deserialize_with = "string_as_f64")]
     pub altitude: f64,
 }
 
@@ -31,6 +34,8 @@ impl Location {
         } else {
             self.latitude
         }
+
+    
     }
 
     pub fn relative_longitude(&self) -> f64 {
@@ -54,6 +59,38 @@ impl Location {
             self.longitude + 360.0
         } else {
             self.longitude
+        }
+    }
+}
+
+fn string_as_f64<'de, D>(deserializer: D) -> Result<f64, D::Error>
+where
+    D: Deserializer<'de>,
+{
+    deserializer.deserialize_f64(F64Visitor)
+}
+
+struct F64Visitor;
+impl<'de> Visitor<'de> for F64Visitor {
+    type Value = f64;
+    fn expecting(&self, formatter: &mut fmt::Formatter) -> fmt::Result {
+        formatter.write_str("a string representation of a f64")
+    }
+    fn visit_str<E>(self, value: &str) -> Result<f64, E>
+    where
+        E: de::Error,
+    {
+        let float_result = value.parse::<f64>();
+        let int_result = value.parse::<i64>();
+
+        match (&float_result, &int_result) {
+            (_, Ok(val)) => {
+                let float_val = *val as f64;
+                Result::Ok(float_val)
+            },
+            _ => float_result.map_err(|_err| {
+                E::invalid_value(Unexpected::Str(value), &"a string representation of a f64")
+            })
         }
     }
 }
