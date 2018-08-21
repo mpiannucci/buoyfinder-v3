1fbd760251acc9c1469901747f978e4d1352b34d
Matthew Iannucci <miannucci@navatekltd.com>
2018-08-21 13:49:46 -0400

    Simple working parse of buoy stations with reqweust

--- cargo/src/lib.rs
+++ cargo/src/lib.rs
@@ -22,17 +22,32 @@ pub mod ffi;
 mod tests {
     use std::fs::File;
     use std::io::Read;
+    use std;
     use std::path::Path;
     use station;
+    use reqwest;
 
     #[test]
-    fn read_stations_xml() {
-        let stations_xml_path = Path::new("mock/activestations.xml");
-        let mut stations_xml_file = File::open(stations_xml_path).expect("file not found");
-        let mut raw_station_data = String::new();
-        stations_xml_file.read_to_string(&mut raw_station_data).unwrap();
-        let buoy_stations: station::BuoyStations = station::BuoyStations::from_raw_data(raw_station_data.as_ref());
+    fn fetch_stations_xml() {
+        let mut res = reqwest::get("https://ndbc.noaa.gov/activestations.xml").unwrap();
+        println!("Status: {}", res.status());
+        println!("Headers:\n{:?}", res.headers());
+
+        let buoy_stations: station::BuoyStations = station::BuoyStations::from_raw_data(res.text().unwrap().as_ref());
         println!("{:?}", buoy_stations);
-        assert_eq!(buoy_stations.station_count, buoy_stations.stations.len() as i64 - 1)
+        assert_eq!(buoy_stations.station_count, buoy_stations.stations.len() as i64 - 1);
+
+        println!("\n\nDone.");
     }
+
+    // #[test]
+    // fn read_stations_xml() {
+    //     let stations_xml_path = Path::new("mock/activestations.xml");
+    //     let mut stations_xml_file = File::open(stations_xml_path).expect("file not found");
+    //     let mut raw_station_data = String::new();
+    //     stations_xml_file.read_to_string(&mut raw_station_data).unwrap();
+    //     let buoy_stations: station::BuoyStations = station::BuoyStations::from_raw_data(raw_station_data.as_ref());
+    //     println!("{:?}", buoy_stations);
+    //     assert_eq!(buoy_stations.station_count, buoy_stations.stations.len() as i64 - 1)
+    // }
 }
