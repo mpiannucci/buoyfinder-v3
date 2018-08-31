588d596b16e1d1cfff42d3fe24da18746c4852cb
Matthew Iannucci <mpiannucci@gmail.com>
2018-08-31 11:58:00 -0400

    Add store kotlin bindings

--- cargo/bindings/java/com/mpiannucci/buoyfinder/core/Store.kt
+++ cargo/bindings/java/com/mpiannucci/buoyfinder/core/Store.kt
@@ -1,1 +1,19 @@
-// Test
+package com.mpiannucci.buoyfinder.core
+
+class Store {
+
+    private val ptr: Long
+
+    init {
+        ptr = new()
+    }
+
+    fun finalize() {
+        free(ptr)
+    }
+
+    public external fun fetch_buoy_stations()
+
+    private external fun new(): Long
+    private external fun free(ptr: Long)
+}
