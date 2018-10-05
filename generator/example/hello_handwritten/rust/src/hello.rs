9ec849b2caff8c8387d299559ac08e94a357a7d6
Matthew Iannucci <miannucci@navatekltd.com>
2018-10-05 15:23:01 -0400

    Add jni handwritten example bindings

--- generator/example/hello_handwritten/rust/src/hello.rs
+++ generator/example/hello_handwritten/rust/src/hello.rs
@@ -1,7 +1,11 @@
 use libc::c_void;
 use std::ffi::{CStr, CString};
 use std::os::raw::c_char;
-use super::sort_items;
+use jni::{JavaVM, JNIEnv};
+use jni::objects::{JClass, JString, JValue, JObject, GlobalRef};
+use jni::sys::{jint, jlong, jdouble, jboolean, jstring};
+
+use super::sort_items::SortItems;
 
 #[no_mangle]
 pub fn c_char_to_string(cchar: *const c_char) -> String {
@@ -40,6 +44,25 @@ pub unsafe extern fn item_list_items_index(item_list: *mut ItemList, index: i32)
     string_to_c_char(item_list.items[index as usize].clone())
 }
 
+#[no_mangle]
+pub unsafe extern fn Java_com_mpiannucci_hello_ItemList_free(_: JNIEnv, _: JClass, ptr: jlong) {
+    let item_list = ptr as *mut ItemList;
+    item_list_free(item_list);
+}
+
+#[no_mangle]
+pub unsafe extern fn Java_com_mpiannucci_hello_ItemList_itemsCount(_: JNIEnv, _: JClass, ptr: jlong) -> jint {
+    let item_list = ptr as *mut ItemList;
+    item_list_items_count(item_list) as jint
+}
+
+#[no_mangle]
+pub unsafe extern fn Java_com_mpiannucci_hello_ItemList_itemsIndex(env: JNIEnv, _: JClass, ptr: jlong, index: i32) -> jstring  {
+    let item_list = ptr as *mut ItemList;
+    let output = env.new_string((*item_list).items[index as usize].as_str()).expect("Failed to create itemsIndex string");
+    output.into_inner()
+}
+
 pub trait ItemsListener {
     fn update(&mut self, items: &ItemList);
 }
@@ -61,12 +84,53 @@ impl ItemsListener for ItemsListenerWrapper {
     }
 }
 
+struct ItemsListenerJVMWrapper {
+    jvm: JavaVM,
+    listener: GlobalRef,
+}
+
+impl ItemsListener for ItemsListenerJVMWrapper {
+    fn update(&mut self, items: &ItemList) {
+        let item_list = Box::into_raw(Box::new(items.clone()));
+        if let Ok(env) = self.jvm.get_env() {
+            let j_items_list_class = env.find_class("com/mpiannucci/hello/ItemsList")
+                .expect("Failed to find ItemsList class");
+            let j_items_list = env.new_object(j_items_list_class, "(J)V", &[JValue::Long(item_list as jlong).into()])
+                .expect("Failed to create a ItemsList jvm object");
+            let j_listener = self.listener.as_obj();
+            env.call_method(j_listener, "update", "(Lcom/mpiannucci/hello/ItemList;)V", &[JValue::Object(j_items_list).into()])
+                .expect("Failed to call update on the JVM receiver");
+        } else {
+            let env = self.jvm.attach_current_thread()
+                .expect("Failed to attach to the current thread");
+            let j_items_list_class = env.find_class("com/mpiannucci/hello/ItemsList")
+                .expect("Failed to find ItemsList class");
+            let j_items_list = env.new_object(j_items_list_class, "(J)V", &[JValue::Long(item_list as jlong).into()])
+                .expect("Failed to create a ItemsList jvm object");
+            let j_listener = self.listener.as_obj();
+            env.call_method(j_listener, "update", "(Lcom/mpiannucci/hello/ItemList;)V", &[JValue::Object(j_items_list).into()])
+                .expect("Failed to call update on the JVM receiver");
+        }
+    }
+}
+
 #[derive(Clone, Debug)]
 #[repr(C)]
 pub enum SortOrder {
     Ascending,
     Descending,
     Random,
+}
+
+impl From<i32> for SortOrder {
+    fn from(value: i32) -> SortOrder {
+        match value {
+            0 => SortOrder::Ascending,
+            1 => SortOrder::Descending,
+            2 => SortOrder::Random,
+            _ => SortOrder::Ascending,
+        }
+    }
 }
 
 pub trait SortItemsInterface {
@@ -76,23 +140,58 @@ pub trait SortItemsInterface {
 }
 
 #[no_mangle]
-pub unsafe extern fn sort_items_new(listener: ItemsListenerContainer) -> *mut sort_items::SortItems {
+pub unsafe extern fn sort_items_new(listener: ItemsListenerContainer) -> *mut SortItems {
     let listener = Box::new(ItemsListenerWrapper{
         listener: listener,
     });
 
-    Box::into_raw(Box::new(sort_items::SortItems::new(listener)))
+    Box::into_raw(Box::new(SortItems::new(listener)))
 }
 
 #[no_mangle]
-pub unsafe extern fn sort_items_sort(sort_items: *mut sort_items::SortItems, order: SortOrder, items: *mut ItemList) {
+pub unsafe extern fn sort_items_free(sort_items: *mut SortItems) {
+    let _ = Box::from_raw(sort_items);
+}
+
+#[no_mangle]
+pub unsafe extern fn sort_items_sort(sort_items: *mut SortItems, order: SortOrder, items: *mut ItemList) {
     let sort_items = &mut*sort_items;
     let items = &mut*items;
     sort_items.sort(&order, &items);
 }
 
 #[no_mangle]
-pub unsafe extern fn sort_items_listener_count(sort_items: *mut sort_items::SortItems) -> i32 {
+pub unsafe extern fn sort_items_listener_count(sort_items: *mut SortItems) -> i32 {
     let sort_items = &mut*sort_items;
     sort_items.listener_count()
+}
+
+#[no_mangle]
+pub unsafe extern fn Java_com_mpiannucci_hello_SortItems_new(env: JNIEnv, _: JClass, listener: JObject<'static>) -> jlong {
+    let listener = Box::new(ItemsListenerJVMWrapper{
+        jvm: env.get_java_vm().expect("Failed to get the JVM"),
+        listener: env.new_global_ref(listener).expect("Failed to get a global ref from listener"),
+    });
+
+    Box::into_raw(Box::new(SortItems::new(listener))) as jlong
+}
+
+#[no_mangle]
+pub unsafe extern fn Java_com_mpiannucci_hello_SortItems_free(_: JNIEnv, _: JClass, ptr: jlong) {
+    let sort_items = ptr as *mut SortItems;
+    sort_items_free(sort_items);
+}
+
+#[no_mangle]
+pub unsafe extern fn Java_com_mpiannucci_hello_SortItems_sort(_: JNIEnv, _: JClass, ptr: jlong, order: i32, items: jlong) {
+    let sort_items = ptr as *mut SortItems;
+    let order = SortOrder::from(order);
+    let items = items as *mut ItemList;
+    sort_items_sort(sort_items, order, items);
+}
+
+#[no_mangle]
+pub unsafe extern fn Java_com_mpiannucci_hello_SortItems_listenerCount(_: JNIEnv, _: JClass, ptr: jlong) -> jint {
+    let sort_items = ptr as *mut SortItems;
+    sort_items_listener_count(sort_items) as jint
 }
